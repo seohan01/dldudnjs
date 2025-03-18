bd1f7f9b6f5b305f6a548574ebee5656f32a74f2
seohan01 <dldudnjs0111@naver.com>
2025-03-19 00:23:49 +0900

    Title: 따옴표 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250319/따옴표 출력/print-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char sentence1[50] = "He says \"It's a really simple sentence\".";
+    //char sentence2[40] = "\"It's a really simple sentence\".";
+
+    printf(sentence1);
+
+    return 0;
+}
